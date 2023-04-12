8c499dfcad2f873aaceb83a9fe69569ec6e5ef92
JacksonFang <a3322683@gmail.com>
2023-04-12 20:14:42 +0800

    test

---
+++ practice.py
@@ -0,0 +1,13 @@
+def add_three_nums(num1, num2, num3):
+    return num1 + num2 + num3
+
+def main():
+    num1 = 3
+    num2 = 4
+    num3 = 5
+
+    result = add_three_nums(num1, num2, num3)
+    print(result)  # Output: 12
+
+if __name__ == '__main__':
+    main()
