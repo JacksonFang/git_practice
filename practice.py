072512cc70618c41084c528fd3d5a056c978d397
JacksonFang <a3322683@gmail.com>
2023-04-12 21:09:50 +0800

    4aaa

--- practice.py
+++ practice.py
@@ -7,6 +7,12 @@ def test2(num1, num2, num3):
 def test1(num1, num2, num3):
     return num1 + num2 + num3
 
+def test42(num1, num2, num3):
+    return num1 + num2 + num3
+
+def test41(num1, num2, num3):
+    return num1 + num2 + num3
+
 def main():
     num1 = 3
     num2 = 4
