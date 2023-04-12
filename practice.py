bfd70c4d3dd02d431f25d34c871132f762b97304
JacksonFang <a3322683@gmail.com>
2023-04-12 21:10:04 +0800

    4444aaa

--- practice.py
+++ practice.py
@@ -25,3 +25,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def test45(num1, num2, num3):
+    return num1 + num2 + num3
+
+def test46(num1, num2, num3):
+    return num1 + num2 + num3
+
+def test41(num1, num2, num3):
+    return num1 + num2 + num3
